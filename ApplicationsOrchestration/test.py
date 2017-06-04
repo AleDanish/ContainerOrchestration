b5d1e2e6d1bda30863ccef273c3e176f7d6e9b22
Alessandro <superalle87@gmail.com>
2017-06-04 18:29:55 +0100

    web server - compose file modification

--- ApplicationsOrchestration/test.py
+++ ApplicationsOrchestration/test.py
@@ -1,6 +1,7 @@
 import fileinput
 import tornado.ioloop
 import tornado.web
+import re
 
 WEB_SERVER_PORT = 8888
 #hostname_list = ["raspberry1", "raspberry2"]
@@ -9,10 +10,26 @@ docker_compose_file = "docker-compose.yml"
 
 #curl -d hostname=alessandro-VirtualBox http://10.101.101.119:8888
 
+def count_start_spaces(string):
+    count = 0
+    for i in string:
+        if i == ' ':
+            count += 1
+        else:
+            return count
+
 def edit_deploy_settings(hostname):
+    host_number = re.search(r'\d+$', hostname).group(0)
     for line in fileinput.input(docker_compose_file, inplace=True):
         line = line.replace("\n","")
-        if "node.hostname" in line:
+        if count_start_spaces(line) == 2:
+            service_name = line.split(':')[0]
+            service_number = re.search(r'\d+$', service_name)
+            if service_number is not None:
+                service_number = service_number.group(0)
+                service_name = service_name[:-len(service_number)]
+            print(service_name + host_number + ":")
+        elif "node.hostname" in line:
             print(line.split("==")[0] + "==" + hostname)
         else:
             print(line)
@@ -20,9 +37,7 @@ def edit_deploy_settings(hostname):
 class MainHandler(tornado.web.RequestHandler):
     def post(self):
         arguments = self.request.arguments
-        
         hostname_request = arguments["hostname"]
-        
         for hostname in hostname_list:
             if hostname != hostname_request:
                 hostname_receiver = hostname
