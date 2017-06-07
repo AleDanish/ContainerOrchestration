7406ff00d1c9ee021316cbf75a2e2101872e2a99
Alessandro <alessandrozanni.dev@gmail.com>
2017-06-07 16:12:05 +0200

    print output test applications

--- TestPy/test.py
+++ TestPy/test.py
@@ -6,22 +6,25 @@ port_mongo = "27017"
 db = "testDB"
 collection = "user"
 
+print("Testing Mosquitto...")
 client = MQTTClient(ip, "client1")
 client.connect()
 client.subscribe("test/data1")
 client.publish("test/data1", "MESSAGE")
 client.log()
 client.disconnect()
+print("Tested Mosquitto successfully")
 
 #time.sleep(5)
 #client.publish("test/data1", "MESSAGE")
 
-
-#clientDB = DBManager.connect(ip, port_mongo)
-#DBManager.initilize_collection(clientDB, db, collection)
-#data = [{"_id" : 1, "foo" : "Hello"}, {"_id" : 2, "foo2" : "Bye"}]
-#DBManager.insert_data(clientDB, db, collection, data)
-#cursor = DBManager.read_document(clientDB, db, collection, {})
-#for doc in cursor:
-#    print(doc)
-#DBManager.remove_document(clientDB, db, collection, "foo", "Hello")
+print("Testing MongoDB...")
+clientDB = DBManager.connect(ip, port_mongo)
+DBManager.initilize_collection(clientDB, db, collection)
+data = [{"_id" : 1, "foo" : "Hello"}, {"_id" : 2, "foo2" : "Bye"}]
+DBManager.insert_data(clientDB, db, collection, data)
+cursor = DBManager.read_document(clientDB, db, collection, {})
+for doc in cursor:
+    print(doc)
+DBManager.remove_document(clientDB, db, collection, "foo", "Hello")
+print("Tested MongoDB successfully")
