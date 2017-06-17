bb105eb9eddfe91ef3351b7a65451ace1137ee82
Alessandro <alessandrozanni.dev@gmail.com>
2017-06-17 03:11:11 +0200

    Fog node resources/hotspot monitoring

--- FogMonitoring/fog_management.py
+++ FogMonitoring/fog_management.py
@@ -2,45 +2,14 @@
 #     <alessandro.zanni3@unibo.it>
 
 import monitoring
-import time
-import threading
-import subprocess
+import hostapd
 
 #curl -d hostname=alessandro-VirtualBox2 -d mode=mobile_presence -d mac=arduino_mac1 http://192.168.56.101:8888
 #curl -d hostname=alessandro-VirtualBox2 -d mode=scale_up http://192.168.56.101:8888
 #curl -d hostname=alessandro-VirtualBox2 -d mode=scale_down http://192.168.56.101:8888
 
-MONITORING_TIMEFRAME = 10
-HOSTAPD_TIMEFRAME = 10
-
-class myThread_Monitoring(threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-    def run(self):
-        while True:
-            _cpu, _mem, _disk = monitoring.monitoring_resource()
-            print('CPU (' + str(_cpu) + '%) - MEM (' + str(_mem.percent) + '%) - DISK (' + str(_disk.percent) + '%)')
-            time.sleep(MONITORING_TIMEFRAME)
-
-class myThread_Hostapd(threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-    def run(self):
-        while True:
-            cmd = "hostapd_cli all_sta"
-            proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-            for line in iter(proc.stdout.readline,''):
-                line = line.decode("utf-8").replace("\n","")
-                break
-            time.sleep(HOSTAPD_TIMEFRAME)
-
-
 # Create threads
-thread1 = myThread_Monitoring(1, "Thread-Monitoring")
-thread2 = myThread_Hostapd(2, "Thread-Hostapd")
+thread1 = monitoring.myThread_Monitoring(1, "Thread-Monitoring", 1)
+thread2 = hostapd.myThread_Hostapd(2, "Thread-Hostapd", 2)
 thread1.start()
 #thread2.start()
--- FogMonitoring/monitoring.py
+++ FogMonitoring/monitoring.py
@@ -2,13 +2,32 @@
 #     <alessandro.zanni3@unibo.it>
 
 import psutil
+import threading
+import time
 
-def monitoring_resource():
-    while True:
-        """ Return physical cpu usage """
-        _cpu=psutil.cpu_percent()
-        """ Return physical memory usage """
-        _mem= psutil.virtual_memory()
-        """ Return physical disk usage """
-        _disk=psutil.disk_usage("/")
-        return _cpu, _mem, _disk 
+MONITORING_FILE = "dataset.txt"
+MONITORING_TIMEFRAME = 10
+
+class myThread_Monitoring(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def monitoring_resource(self):
+        while True:
+            """ Return physical cpu usage """
+            _cpu=psutil.cpu_percent()
+            """ Return physical memory usage """
+            _mem= psutil.virtual_memory().percent
+            """ Return physical disk usage """
+            _disk=psutil.disk_usage("/").percent
+            print('CPU (' + str(_cpu) + '%) - MEM (' + str(_mem) + '%) - DISK (' + str(_disk) + '%)')
+            return _cpu, _mem, _disk
+    def write_file(self, _cpu, _mem, _disk):
+        with open(MONITORING_FILE, "a+") as file:
+            file.write("CPU:" + str(_cpu) + " MEM:" + str(_mem) + " DISK:" + str(_disk) + "\n")
+    def run(self):
+        while True:
+            _cpu, _mem, _disk = self.monitoring_resource()
+            self.write_file(_cpu, _mem, _disk)
+            time.sleep(MONITORING_TIMEFRAME)
