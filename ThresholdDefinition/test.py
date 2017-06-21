6c4eb998d98ffa02320d81c4ff5cd6c456e7a892
Alessandro <alessandrozanni.dev@gmail.com>
2017-06-21 12:14:14 +0200

    Function Opitimization

--- ThresholdDefinition/test.py
+++ ThresholdDefinition/test.py
@@ -1,50 +1,58 @@
-print(__doc__)
-
-
-# Code source: Jaques Grobler
-# License: BSD 3 clause
-
-
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
-#import sklearn
+import scipy.optimize
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
+def our_function(coeff, data):
+    A, B, C = coeff # parameters to optimize
+    x, y, z = data.T # input data
+    return A*x+B*y+C #least squared function (funzione dei minimi quadrati)
 
+# FORMULATION #2: a special least squares problem
+def least_squares_residuals(coeff, data, target):
+    """
+        Function that returns the vector of residuals between the predicted values
+        and the target value. Here we want each predicted value to be close to zero
+    """
+    prediction = our_function(coeff, data)
+    vector_of_residuals = (prediction - target)
+    return vector_of_residuals
 
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
+data = np.loadtxt('dataset.txt')
+input_data = np.array([data[:,(0)], data[:,(1)],np.ones([20])]).T
+target = data[:,(2)]
+num_coeff = 3
+coeff_0 = np.ones(num_coeff)
 
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+lst_sqrs_result = scipy.optimize.least_squares(least_squares_residuals, coeff_0, args=(input_data, target))
+# Test what the squared error of the returned result is
+coeff = lst_sqrs_result.x
+lst_sqrs_output = our_function(coeff, data)
+print('====================')
+print('lst_sqrs_result =\n%s' % (lst_sqrs_result,))
+print('---------------------')
+print('lst_sqrs_output = %r' % (lst_sqrs_output,))
+print('====================')
 
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
+# plot raw data
+plt.figure()
+ax = plt.subplot(111, projection='3d')
+ax.scatter(data[:,(0)], data[:,(1)], data[:,(2)], color='b')
 
-# Create linear regression object
-regr = linear_model.LinearRegression()
+# plot plane
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]), np.arange(ylim[0], ylim[1]))
+Z = np.zeros(X.shape)
+A = lst_sqrs_result.x[0]
+B = lst_sqrs_result.x[1]
+C = lst_sqrs_result.x[2]
+for r in range(X.shape[0]):
+    for c in range(X.shape[1]):
+        Z[r,c] = (A * X[r,c] + B * Y[r,c] + C)
+ax.plot_wireframe(X,Y,Z, color='k')
 
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
-
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
-
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
-         linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
 plt.show()
