69193b4f20fdd72cdf67449630926f7d6129be0e
Alessandro <alessandrozanni.dev@gmail.com>
2017-06-26 22:53:50 +0200

    cross-arch images creation

--- RaspberryConfiguration/test-app-raspi/MQTTClient.py
+++ RaspberryConfiguration/test-app-raspi/MQTTClient.py
@@ -4,19 +4,24 @@ import time
 class MQTTClient:
     broker_address=""
     client=None
+    messages=[]
     
     def __init__(self, ip, client_name):
         self.broker_address=ip
         self.client = mqtt.Client(client_name) #create new instance
         self.client.on_connect=self.on_connect #attach function to callback
         self.client.on_message=self.on_message #attach function to callback
-            
+        self.messages=[]
+
     def on_connect(self, client, userdata, flags, rc):
         m="Connected flags"+str(flags)+"; result code "+str(rc)+"; client_id  "+str(client)
-        print(m)
+        #print(m)
     
     def on_message(self, client, userdata, message):
-        print("Message received  ", str(message.payload.decode("utf-8")))
+        msg = str(message.payload.decode("utf-8"))
+        topic = message.topic
+        print("Message received  "+msg + " on topic " + topic)
+        self.messages.append(topic+":"+msg)
     
     def on_log(self, client, userdata, level, buf):
         print("log: ",buf)
---
+++ RaspberryConfiguration/test-app-raspi/test.py
@@ -0,0 +1,80 @@
+import DBManager
+from MQTTClient import MQTTClient
+import socket
+import fcntl
+import struct
+import time
+
+#def get_external_ip_address():
+#    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#    s.connect(("8.8.8.8", 80))
+#    return s.getsockname()[0]
+
+def get_ip_address_from_interface(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
+
+#ip = get_ip_adress()
+#ip=get_ip_address_from_interface("docker0")
+ip = "172.17.0.1"
+#ip="127.0.0.1"
+port_mongo = "27017"
+db = "test"
+collection = "user"
+LOG_FILE = "log.txt"
+
+TOPIC="/test/fogorchestration"
+TIMEFRAME = 10
+print("IP: " + ip)
+
+client = MQTTClient(ip, "client1")
+client.connect()
+client.subscribe(TOPIC)
+print("Subscribed to topic " + TOPIC)
+print("MQTT broker listening on port 1883")
+#client.publish("test/data1", "MESSAGE")
+client.log()
+
+def write(option, data):
+    with open(LOG_FILE, option) as file:  # @ReservedAssignment
+        file.write(data)
+    file.close()
+
+write("w", "Subscribed to topic " + TOPIC + "\n")
+write("a+", "MQTT broker listening on port 1883 \n")
+
+count=0
+while True:
+    messages = client.messages
+    if len(messages) > 0:
+        client.messages = []
+        clientDB = DBManager.connect(ip, port_mongo)
+        DBManager.initilize_collection(clientDB, db, collection)
+        info = {}
+        data = []
+        for message in messages:
+            info['id'] = count
+            info['information'] = message
+            data.append(info)
+            count+=1
+        DBManager.insert_data(clientDB, db, collection, data)
+        print("Inserted data into the db "+ db + " collection " + collection)
+        write("a+", "Inserted data into the db "+ db + " collection " + collection+ "\n")
+
+        cursor = DBManager.read_document(clientDB, db, collection, {})
+        for doc in cursor:
+            print("I'm reading from datatabase " + db + " collection " + collection)
+            print(doc)
+            write("a+", "I'm reading from datatabase " + db + " collection " + collection+ "\n")
+            write("a+", str(doc) + "\n")
+    time.sleep(TIMEFRAME)
+
+#client.disconnect()
+#print("Tested Mosquitto successfully")
+
+#time.sleep(5)
+#client.publish("test/data1", "MESSAGE")
+#print("Testing MongoDB...")4
+
+#DBManager.remove_document(clientDB, db, collection, "foo", "Hello")
+#print("Tested MongoDB successfully")
