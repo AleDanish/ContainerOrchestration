589bab865cd18b6fbcf2eb027d6a05ea88960cb0
Alessandro <alessandrozanni.dev@gmail.com>
2017-06-26 12:03:29 +0200

    fog node monitoring

--- FogMonitoring/Hostapd.py
+++ FogMonitoring/Hostapd.py
@@ -12,22 +12,30 @@ class myThread_Hostapd(threading.Thread):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.lastDevice = ""
     def get_devices(self):
-        cmd = "hostapd_cli all_sta"
+        cmd = "sudo hostapd_cli all_sta"
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         count = 1
         for line in iter(proc.stdout.readline,''):
             line = line.decode("utf-8").replace("\n","")
             if count == 2:
-                return line            
-            else:
+                return line
+            elif count < 2:
                 count+=1
+            else:
+                break
         return ""
     def run(self):
         while True:
-            #device_mac = self.get_devices()
-            
-            # se device nouvo...contatta il cloud
-            Messages.send("mobile_presence", mac="mac_address")
-            
-            time.sleep(Config.HOSTAPD_TIMEFRAME)
+            device_mac = self.get_devices()
+            print("device: " + device_mac)
+            if (device_mac != "") and (self.lastDevice == ""):
+                print("Device MAC detected:" + device_mac)
+                print("Send a cloud request for mobile presence")
+                Messages.send("mobile_presence", mac="mac_address")
+            elif (device_mac != self.lastDevice) and (device_mac == ""):
+                print("Lost device")
+            self.lastDevice = device_mac
+                
+        time.sleep(Config.HOSTAPD_TIMEFRAME)
