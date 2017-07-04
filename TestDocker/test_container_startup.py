273bc41246dcf931660c1f183228c9af411aba16
alessandro <alessandrozanni.dev@gmail.com>
2017-07-04 18:14:28 +0000

    tests images and containers

--- TestDocker/test_container_startup.py
+++ TestDocker/test_container_startup.py
@@ -3,25 +3,21 @@ import subprocess
 from MQTTClient import MQTTClient
 
 def run_container():
-    cmd = "docker run -p 1883:1883 -p 9001:9001 toke/mosquitto"
+    cmd = "docker run -p 1883:1883 -p 9001:9001 alez/mosquitto-raspi"
     subprocess.Popen(cmd, shell=True)
-
-startDeploy = time.time()
-run_container()
-endDeploy = time.time() 
-timeDeploy = endDeploy - startDeploy
-print("Total time to deploy the container is " + str(timeDeploy) + " s \n")
 
 ip = "172.17.0.1"
 client = MQTTClient(ip, "client1")
-startup = False
+startDeploy = time.time()
+run_container()
+
 while True:
     try:
         client.connect()
         break
     except:
         pass
-timeStartup = time.time() - endDeploy
+timeStartup = time.time() - startDeploy
 
 client.disconnect()
-print("Total time to startup the container is " + str(timeStartup) + " s \n")
+print("Total time to create and startup the container is " + str(timeStartup) + " s \n")
